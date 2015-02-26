c1b39f7ea6188ea3babbdc72f2bb24651e507cc7
jzzhu <jzzhu@jzzhu-desktop.(none)>
2015-02-26 11:17:48 +0800

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,3 +37,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+SampleApp::Application.configure do
+  ActiveModel::SecurePassword.min_cost = true
+end
